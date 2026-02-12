62e86beace1bc35f3b8da2efbe59195a27ddac09
ZacharyLaw <48851717+ZacharyLaw@users.noreply.github.com>
2026-02-12 02:05:10 +0000

    turbopack

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ const withPWA = require('next-pwa')({
 	dest: 'public',
 	register: true,
 	skipWaiting: true,
+	turbopack:{},
+
 })
 
 module.exports = withPWA({
 	reactStrictMode: true,
+	turbopack:{},
 })
